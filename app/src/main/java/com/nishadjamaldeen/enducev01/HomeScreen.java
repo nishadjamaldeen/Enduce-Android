3826d97ffa52410b8ef2c9433dbed670a8a5ad08
nishadjamaldeen <anjamaldeen@alumni.ubc.ca>
2018-01-22 18:26:27 -0800

    Adding nav bar

--- app/src/main/java/com/nishadjamaldeen/enducev01/HomeScreen.java
+++ app/src/main/java/com/nishadjamaldeen/enducev01/HomeScreen.java
@@ -1,8 +1,10 @@
 package com.nishadjamaldeen.enducev01;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,12 +18,15 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
 
     }
 
     public void changeTreatment(View view){
-        Log.e(TAG, "Clicked!");
+        Intent intent = new Intent(this, changetreatment.class);
+        startActivity(intent);
 
     }
 
